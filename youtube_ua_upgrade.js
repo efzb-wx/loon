54bb7ae3688b970c0fcf63dfee15046eb21615a5
efzb-wx <efzbwx@gmail.com>
2026-01-12 14:31:24 +0800

    Update youtube_ua_upgrade.js

--- youtube_ua_upgrade.js
+++ youtube_ua_upgrade.js
@@ -1,36 +1,19 @@
 /**
- * Loon Plugin
- * 功能：升级 YouTube User-Agent 版本号
- * 支持：
- *  - YouTube/17.39.4
- *  - com.google.ios.youtube/17.39.4
+ * Compatible with Loon 2.2.0
+ * Request Header Modify
  */
 
-const TARGET_VERSION = "21.02.3";
-
-let headers = $request.headers;
-if (!headers) {
-  $done({});
-  return;
-}
-
-// 兼容大小写
-let uaKey = headers["User-Agent"] ? "User-Agent" : "user-agent";
-let ua = headers[uaKey];
+var headers = $request.headers || {};
+var ua = headers["User-Agent"];
 
 if (ua) {
-  let newUA = ua
-    // 匹配 YouTube/17.39.4
-    .replace(/(YouTube\/)(\d+(\.\d+)+)/gi, `$1${TARGET_VERSION}`)
-    // 匹配 com.google.ios.youtube/17.39.4
-    .replace(/(com\.google\.ios\.youtube\/)(\d+(\.\d+)+)/gi, `$1${TARGET_VERSION}`);
+  // 替换 YouTube/xx.xx.xx
+  ua = ua.replace(/YouTube\/\d+(\.\d+)+/i, "YouTube/21.02.3");
 
-  if (newUA !== ua) {
-    headers[uaKey] = newUA;
-    console.log("YouTube UA upgraded");
-    console.log("OLD: " + ua);
-    console.log("NEW: " + newUA);
-  }
+  // 替换 com.google.ios.youtube/xx.xx.xx
+  ua = ua.replace(/com\.google\.ios\.youtube\/\d+(\.\d+)+/i, "com.google.ios.youtube/21.02.3");
+
+  headers["User-Agent"] = ua;
 }
 
-$done({ headers });
+$done({ headers: headers });
