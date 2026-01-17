9c3f87aef1b4c08b9e62f6dc509ebd53ad0a4148
efzb-wx <efzbwx@gmail.com>
2026-01-17 17:09:23 +0800

    Update HBReplace.js

--- SKR/HBReplace.js
+++ SKR/HBReplace.js
@@ -1,35 +1,16 @@
-const url = $request.url;
+// Loon response rewrite script
 
-// 仅保险判断一次
-if (url.startsWith("https://api.hb174.top:8081/skrman/servlet/UserSignV2")) {
-
-  const body = {
-    "ret": 200,
-    "data": {
-      "code": 0,
-      "msg": "0",
-      "info": [
-        {
-          "is_see": 0,
-          "type": "0",
-          "type_val": "0",
-          "type_msg": "0",
-          "live_type": "0",
-          "live_sdk": "0"
-        }
-      ]
-    },
-    "msg": ""
-  };
-
-  $done({
-    status: 200,
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify(body)
+if ($response && $response.body) {
+  const newBody = JSON.stringify({
+    e: 0,
+    r: {
+      a: "967"
+    }
   });
 
+  $done({
+    body: newBody
+  });
 } else {
   $done({});
 }
