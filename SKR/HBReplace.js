4dbf5b73c110a2f9b7d3f2ddfeb2291640143adb
efzb-wx <efzbwx@gmail.com>
2026-01-17 17:03:47 +0800

    Update HBReplace.js

--- SKR/HBReplace.js
+++ SKR/HBReplace.js
@@ -1,25 +1,26 @@
-/**
- * Loon http-response script
- * Replace response body for specific URL prefixes
- */
-
 const url = $request.url;
 
-if (url.startsWith("https://api.hb174.top:8081/skrman/servlet/UserSignV2")) {{
-  const body = {
- ={"errcode":0,"result":{"auth":"9e64b5342a26b6d1ef50b3ae3d89d67"}};
-}} 
+// 仅保险判断一次
+if (url.startsWith("https://api.hb174.top:8081/skrman/servlet/UserSignV2")) {
 
-else if (url.startsWith("https://api.hb174.top:8081/skrman/servlet/UserCheckV4")) {
   const body = {
- ={"errcode":0,"result":"ok"};
-}} 
-
-else if (url.startsWith("https://api.hb174.top:8081/skrman/servlet/UserInfoGet")) {
-  const body = {
- = {"errcode":0,"result":{"MobilePhone":"未绑定手机","Vip":"super","VipInfo":"高级会员,2099-12-31到期","ID":666666,"Valid":"2099-12-31","NickName":"新达人6666","Avatar":"https://lazy-1252169987.cos.ap-guangzhou.myqcloud.com/default-avatar.png","Email":"66666666@qq.com"}};
-}
-}
+    "ret": 200,
+    "data": {
+      "code": 0,
+      "msg": "0",
+      "info": [
+        {
+          "is_see": 0,
+          "type": "0",
+          "type_val": "0",
+          "type_msg": "0",
+          "live_type": "0",
+          "live_sdk": "0"
+        }
+      ]
+    },
+    "msg": ""
+  };
 
   $done({
     status: 200,
