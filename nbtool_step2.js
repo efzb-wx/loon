e0ec492ab48174ae5a074b9f677ca5b518c9dda0
efzb-wx <efzbwx@gmail.com>
2026-02-21 17:09:04 +0800

    Update nbtool_step2.js

--- nbtool_step2.js
+++ nbtool_step2.js
@@ -5,11 +5,17 @@
 const url = $request.url;
 
 const REPLACE_KUAISHOU_PREFIX = "https://open.e.kuaishou.com/";
+const REPLACE_ziben_PREFIX = "https://api-dsp.8ziben.com/";
 const REPLACE_ZHANGYU_PREFIX = "http://sdk.zhangyuyidong.cn/";
 
 let body = $response.body || "";
 
 if (url.startsWith(REPLACE_KUAISHOU_PREFIX)) {
+  body = "{}";
+  $done({ body });
+}
+
+if (url.startsWith(REPLACE_ziben_PREFIX)) {
   body = "{}";
   $done({ body });
 }
